be00260c0f4686b3978e64f885c480b62203a36f
MinhMinh24x <loclm9824@gmail.com>
2025-11-20 21:55:51 +0700

    oke

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Nhớ thêm .js vào cuối file import
 import HeroSection from '../components/home/HeroSection.js'; 
 import FeaturedItems from '../components/home/FeaturedItems.js';
-
+//oke luoi qua
 export default function HomePage() {
   return (
     <main>
